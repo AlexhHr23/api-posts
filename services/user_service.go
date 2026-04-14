99722e1e25f9c2990cdf322095f24f4ebd887f1e
AlexhHr23 <alex.hernandez24kb@gmail.com>
2026-04-14 13:12:25 -0600

    validar email y password

--- services/user_service.go
+++ services/user_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"regexp"
 
 	"github.com/AlexhHr23/gopost-api/models"
 	"github.com/AlexhHr23/gopost-api/repositories"
@@ -17,7 +18,34 @@ func NewUserService(repo *repositories.UserRepository) *UserService {
 	return &UserService{repo: repo}
 }
 
+// Validar email
+func ValidateEmail(email string) error {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+
+	if !emailRegex.MatchString(email) {
+		return fmt.Errorf("Formato de email no valido")
+	}
+
+	return nil
+}
+
+func ValidatePasswotd(password string) error {
+	if len(password) < 8 {
+		return fmt.Errorf("La contraseña debe tener al menos 8 caracteres")
+	}
+	return nil
+}
+
 func (s *UserService) SignUp(ctx context.Context, name, email, password string) (*models.User, error) {
+
+	if err := ValidateEmail(email); err != nil {
+		return nil, err
+	}
+
+	if err := ValidatePasswotd(password); err != nil {
+		return nil, err
+	}
+
 	exist, err := s.repo.EmailExist(ctx, email)
 
 	if err != nil {
